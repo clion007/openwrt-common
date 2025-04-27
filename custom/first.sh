90e65347e91652f206678b38050def8f09b9b4a2
Clion.Nieh <clion007@126.com>
2025-04-28 00:19:59 +0800

    修改更新

--- custom/first.sh
+++ custom/first.sh
@@ -42,9 +42,53 @@ elif [[ ! -f "${COMPILE_PATH}/seed/${CONFIG_FILE}" ]]; then
 fi
 }
 
+function Diy_three() {
+LINSHI_COMMON="/tmp/common"
+rm -rf ${LINSHI_COMMON}
+mkdir -p ${LINSHI_COMMON}
+curl -fsSL https://raw.githubusercontent.com/clion007/openwrt-common/main/common.sh -o ${LINSHI_COMMON}/common.sh
+curl -fsSL https://raw.githubusercontent.com/clion007/openwrt-common/main/upgrade.sh -o ${LINSHI_COMMON}/upgrade.sh
+export COMMON_SH="${LINSHI_COMMON}/common.sh"
+export UPGRADE_SH="${LINSHI_COMMON}/upgrade.sh"
+export CONFIG_TXT="${LINSHI_COMMON}/config.txt"
+if grep -q "TIME" "${COMMON_SH}" && grep -q "Diy_Part2" "${UPGRADE_SH}"; then
+  cp -Rf ${COMPILE_PATH} ${LINSHI_COMMON}/${FOLDER_NAME}
+  export DIY_PT1_SH="${LINSHI_COMMON}/${FOLDER_NAME}/diy-part.sh"
+  export DIY_PT2_SH="${LINSHI_COMMON}/${FOLDER_NAME}/diy2-part.sh"
+else
+  TIME r "common文件下载失败"
+  exit 1
+fi
+
+echo "DIY_PT1_SH=${DIY_PT1_SH}" >> ${GITHUB_ENV}
+echo "DIY_PT2_SH=${DIY_PT2_SH}" >> ${GITHUB_ENV}
+echo "COMMON_SH=${COMMON_SH}" >> ${GITHUB_ENV}
+echo "UPGRADE_SH=${UPGRADE_SH}" >> ${GITHUB_ENV}
+echo "CONFIG_TXT=${CONFIG_TXT}" >> ${GITHUB_ENV}
+
+echo '#!/bin/bash' > ${DIY_PT2_SH}
+grep -E '.*export.*=".*"' $DIY_PT1_SH >> ${DIY_PT2_SH}
+chmod +x ${DIY_PT2_SH}
+source ${DIY_PT2_SH}
+
+grep -E 'grep -rl '.*'.*|.*xargs -r sed -i' $DIY_PT1_SH >> ${DIY_PT2_SH}
+sed -i 's/\. |/.\/feeds |/g' ${DIY_PT2_SH}
+grep -E 'grep -rl '.*'.*|.*xargs -r sed -i' $DIY_PT1_SH >> ${DIY_PT2_SH}
+sed -i 's/\. |/.\/package |/g' ${DIY_PT2_SH}
+sed -i 's?./packagefeeds?./feeds?g' ${DIY_PT2_SH}
+grep -vE '^[[:space:]]*grep -rl '.*'.*|.*xargs -r sed -i' $DIY_PT1_SH > tmp && mv tmp $DIY_PT1_SH
+
+echo "OpenClash_branch=${OpenClash_branch}" >> ${GITHUB_ENV}
+echo "Mandatory_theme=${Mandatory_theme}" >> ${GITHUB_ENV}
+echo "Default_theme=${Default_theme}" >> ${GITHUB_ENV}
+chmod -R +x ${OPERATES_PATH}
+chmod -R +x ${LINSHI_COMMON}
+}
+
 function Diy_memu() {
 Diy_one
 Diy_two
+Diy_three
 }
 
 Diy_memu "$@"
