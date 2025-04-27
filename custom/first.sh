9f6d7c94f6e2784e21ad4570fc0cb44bb78a2f88
Clion.Nieh <clion007@126.com>
2025-04-27 23:51:18 +0800

    修改更新

--- custom/first.sh
+++ custom/first.sh
@@ -28,19 +28,14 @@ function Diy_two() {
 cd ${GITHUB_WORKSPACE}
 if [[ ! -d "${OPERATES_PATH}" ]]; then
   TIME r "根目录缺少编译必要文件夹"
-  message="根目录缺少编译必要文件夹"
 elif [[ ! -d "${COMPILE_PATH}" ]]; then
   TIME r "缺少${COMPILE_PATH}文件夹"
-  message="缺少编译必要文件夹"
 elif [[ ! -f "${BUILD_PARTSH}" ]]; then
   TIME r "缺少${BUILD_PARTSH}文件"
-  message="缺少文件"
 elif [[ ! -f "${BUILD_SETTINGS}" ]]; then
   TIME r "缺少${BUILD_SETTINGS}文件"
-  message="缺少文件"
 elif [[ ! -f "${COMPILE_PATH}/relevance/actions_version" ]]; then
   TIME r "缺少relevance/actions_version文件"
-  message="缺少文件"
 elif [[ ! -f "${COMPILE_PATH}/seed/${CONFIG_FILE}" ]]; then
   TIME r "缺少seed/${CONFIG_FILE}文件，请先建立seed/${CONFIG_FILE}文件"
   exit 1
